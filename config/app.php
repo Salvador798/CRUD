bac07767ac61bb2f779b05d860378f20ac9cc36c
Salvador798 <salmartinezv@gmail.com>
2023-12-29 23:58:10 -0400

    CRUD

---
+++ config/app.php
@@ -0,0 +1,8 @@
+<?php
+
+const APP_URL = "http://localhost/PHP/CRUD/";
+const APP_NAME = "CRUD POO MySQL";
+const APP_SESSION_NAME = "CRUD";
+
+/*----------  Zona horaria  ----------*/
+date_default_timezone_set("America/Caracas");
